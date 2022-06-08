5f800ab8f4f3c9a4c1c9e88bbfb21e58046d0b92
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-08 05:29:13 +0200

    customerName

--- src/common/pages/PageCart.jsx
+++ src/common/pages/PageCart.jsx
@@ -3,6 +3,13 @@ import { useSelector } from 'react-redux';
 export const PageCart = () => {
 	const { items } = useSelector((state) => state.cart);
 
+	const formatPrice = (price) => {
+		return new Intl.NumberFormat('de-DE', {
+			style: 'currency',
+			currency: 'EUR',
+		}).format(price);
+	};
+
 	return (
 		<div className="page_cart">
 			<h2>Shopping Cart</h2>
@@ -10,10 +17,12 @@ export const PageCart = () => {
 			<ul>
 				{items.map((item, index) => {
 					return (
-						<li key={index}>{item}</li>
-				)
-			})}
+						<li key={index}>
+							{item.title} - {formatPrice(item.price)}
+						</li>
+					);
+				})}
 			</ul>
 		</div>
-	)
-}
+	);
+};
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,20 +2,24 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
 	items: [],
+	customerName: '',
 };
 
 export const cartSlice = createSlice({
 	name: 'cart',
 	initialState,
 	reducers: {
-		add: (state) => {
-			state.items.push('book');
+		addItem: (state, action) => {
+			state.items.push(action.payload);
 		},
-		subtract: (state) => {
+		removeItem: (state) => {
 			state.items.pop();
 		},
+		changeCustomerName: (state, action) => {
+			state.customerName = action.payload;
+		}
 	},
 });
 
-export const { add, subtract } = cartSlice.actions;
+export const { addItem, removeItem } = cartSlice.actions;
 export default cartSlice.reducer;
